05d8791385c015659b1e549f1a4257d14cda101f
Przemysław Bulanda <devprzemas@gmail.com>
2024-10-02 15:35:09 +0200

    Edit diffrent command build

--- backand/prisma/seed.ts
+++ backand/prisma/seed.ts
@@ -1,53 +1,27 @@
 import { PrismaClient, Role } from '@prisma/client';
+import * as bcrypt from 'bcryptjs';
 const db = new PrismaClient();
 
 function getCarsExports() {
-  return [
-    {
-      id: 'd9994b64-4222-48e5-973b-43a1564de4c8',
-      img: 'hyundai3.jpg',
-      carMark: 'Hyundai9',
-      restImg: JSON.stringify(['hyundai1.jpg', 'hyundai2.jpg', 'hyundai3.jpg']),
-      description: JSON.stringify(['przykład', '1', '2', '2', '4']),
-    },
-  ];
+  return [];
 }
 function getCarsDetailing() {
-  return [
-    {
-      id: 'a1f11b1f-5e2c-43c4-9e16-ba67f8eb5717',
-      img: 'hyundai1.jpg',
-      carMark: 'Hyundai1',
-      restImg: JSON.stringify(['hyundai1.jpg', 'hyundai2.jpg', 'hyundai3.jpg']),
-      description:
-        'Lorem Ipsum is Lorem Ipsum but it is Lorem Ipsum and is therefore not Lorem Ipsum',
-    },
-  ].sort((a, b) => a.id.localeCompare(b.id));
+  return [];
 }
 
 function getMail() {
-  return [
-    {
-      id: '5719cb57-05a8-4392-b1ec-5b64fa2316b8',
-      name: 'John',
-      surname: 'Doe',
-      email: 'user1@example.com',
-      phone: '123456789',
-      title: 'Lorem Ipsum',
-      message:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel semper velit. Duis vel nunc ac nunc tincidunt congue. Donec consectetur velit a justo semper, vitae congue erat molestie.',
-    },
-  ];
+  return [];
 }
-function getUser() {
+async function getUser() {
+  const hashedPassword = await bcrypt.hash('mskAdmin', 10); // Hashowanie hasła
   return [
     {
       id: '5f8d0c9b-bf21-4f2c-8c8d-6c5d8e6dbe7b',
-      email: 'przykładowyemail@gmail.com',
+      email: 'mskcars@gmail.com',
       role: Role.ADMIN,
       password: {
         create: {
-          hashedPassword: 'hashed_password_here',
+          hashedPassword: hashedPassword,
         },
       },
     },
@@ -60,13 +34,13 @@ async function seed() {
   await db.user.deleteMany();
 
   await Promise.all(
-    getCarsDetailing().map((carsExport) => {
-      return db.carsExport.create({ data: carsExport });
+    getCarsDetailing().map((carsDetailing) => {
+      return db.carsDetailing.create({ data: carsDetailing });
     }),
   );
   await Promise.all(
-    getCarsDetailing().map((carsDetailing) => {
-      return db.carsDetailing.create({ data: carsDetailing });
+    getCarsExports().map((carsExport) => {
+      return db.carsExport.create({ data: carsExport });
     }),
   );
   await Promise.all(
@@ -74,8 +48,9 @@ async function seed() {
       return db.mail.create({ data: mail });
     }),
   );
+  const users = await getUser();
   await Promise.all(
-    getUser().map((user) => {
+    users.map((user) => {
       return db.user.create({ data: user });
     }),
   );
